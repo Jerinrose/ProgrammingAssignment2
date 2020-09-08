85601cd7f2a56a250f34217f6010bb84041c4803
Jerinrose <35173645+Jerinrose@users.noreply.github.com>
2020-09-08 15:37:49 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 inv <<- NULL
         }
         get <- function() x
-        setInverse <- function(inversematrix) inv <<- inversematrix
+        setInverse <- function(solveinversematrix) inv <<- solveinversematrix
         getInverse <- function() inv
         list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
